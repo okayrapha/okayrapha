343b25944907bd92d84bc4530b1cfc4fbb89abdd
Rapha <91353927+okayrapha@users.noreply.github.com>
2021-11-08 15:03:04 -0300

    exercicios de estrutura

---
+++ Atividades/exercicio.js
@@ -0,0 +1,6 @@
+// tabuada 
+deixe i  =  0 ;
+enquanto ( i  <=  10 ) {
+    console . log ( 'Tabuada:' ,  i  +  "x"  +  4  +  "="  +  i  *  4 ) ;  
+    i  =  i  +  1 ;
+}
---
+++ Atividades/while.js
@@ -0,0 +1,90 @@
+//FAZER ENQUANTO
+/ ** O DO.WHILE sempre irá EXECUTAR ANTES DE VERIFICAR A CONDIÇÃO * /
+
+let  contador  =  1 ;  // variavel
+
+faça  {
+    console . log ( 'Dentro do DO.while:' ,  contador ) ;
+    contador  =  contador  +  1 ;  // incremento
+}  while  ( contador  <  8 ) // condição
+
+console . log ( "Fora do DO.while:" ,  contador ) ; 
+console . log ( '' ) ;
+
+
+//enquanto
+/ ** Enquanto verifica a condição antes de executar * /
+deixe  contadorr  =  1 ;  // Variavel
+
+while  ( contadorr  <  10 ) {
+    console . log ( 'Dentro do While:' ,  contadorr ) ;
+    contadorr  =  contadorr  +  1 ;
+}
+console . log ( 'Fora do while:' ,  contadorr ) ;
+console . log ( '' ) ;
+
+
+//para 
+// O para terá (variavel; condição; incremento) em um só linha //
+
+para  ( deixe  contador  =  0 ;  contador  <=  5 ;  contador ++ ) {
+    console . log ( 'Dentro do for:' ,  contador ) ;
+}
+
+console . log ( 'fora do for:' ,  contador ) ;
+console . log ( '' )
+
+
+/// Exercitando
+
+//Enquanto
+deixe  nomes  =  [ 'Sarah' ,  'Cauã' ,  'Katia' ,  'Beatriz' ,  'Miguel' ] ;
+deixe  i  =  0 ;
+
+enquanto ( i  <  5 )  {
+    console . log ( 'Nomes:' ,  nomes [ i ]  +  'Aquino' ) ;
+    i  =  i  +  1 ;
+}
+console . log ( '' ) ;
+
+
+// Fazer enquanto
+deixar  alunos  =  [ 'Sarah' ,  'Cauã' ,  'Katia' ,  'Beatriz' ,  'Miguel' ] ;
+deixe  ii  =  0 ;
+
+faça {
+    console . log ( 'Nome do aluno (a):' ,  alunos [ ii ]  +  'Aquino' )
+    ii  =  ii  +  1 ;
+}  enquanto ( ii  <  5 ) ;
+
+console . log ( '' )
+
+
+// para 
+deixar  user  =  [ 'Sarah' ,  'Cauã' ,  'Katia' ,  'Beatriz' ,  'Miguel' ] ; 
+para ( seja  a  =  0 ;  a  <  5 ;  a  =  a  +  1 ) {
+    console . log ( 'Nome do usuario:' ,  usuário [ a ]  +  'Aquino' )
+}
+
+console . log ( '' )
+
+/// TABUADA 
+
+//ENQUANTO 
+
+deixe  b  =  0 ;
+
+enquanto ( b  <=  10 ) {
+    console . log ( 'TABUADA:' ,  tabuada ) ;
+    console . log ( 'Tabuada:' ,  tabuada [ b ] )
+
+}
+
+
+//para 
+
+para  ( seja  k  =  0 ;  k  <=  10 ;  k ++ )  {
+    console . log ( k  +  "x"  +  2  +  "="  +  k  *  2 ) ;
+}
+
+deixe  tabuada  =  [ 1 ,  2 ,  3 ,  4 ,  5 ,  6 ,  7 ,  8 ,  9 ,  10 ] ;
