81b39d9bf20ee2a33a26f916197faceec37ae34c
Rapha <91353927+okayrapha@users.noreply.github.com>
2021-11-12 18:51:50 -0300

    Série Bridgerton

---
+++ Atividades/IMG/aula_string/js/main.js
@@ -0,0 +1,38 @@
+let string 'Instituto da Oportunidade Social';
+
+// Acessando o Primeiro Caracter
+console.log(string.charAt(3));
+console.log(string[0]);
+
+console.clear();
+
+let texto= 'Rafaela';
+console.log (texto.length);
+
+console.clear();
+
+let s = 'Hello World';
+console.log(s.toLocaleUpperCase());
+
+let d = "HELLO WORLD";
+console.log(s.toLowerCase());
+
+console.clear();
+
+let str = "Rafaela";
+console.log(str.substring(0, 4));
+console.log(str.substring(4));
+
+console.clear();
+
+const srtl= 'A raposa é um animal esperto';
+const palavras = srt.split('');
+console.log(palavras[7]),
+
+console.clear();
+
+let mensagem 'Venha para a Microsoft',
+console.log(mensagem);
+
+let novaMensagem = mensagem.replace
+(Microsoft)
